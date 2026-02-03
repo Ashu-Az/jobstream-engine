85785647ec727a9650680e41f8bda2ec184a4646
Ashu-Az <ashuazadblk@gmail.com>
2026-02-03 16:59:00 +0530

    Add TLS support for Redis (Upstash)

--- server/src/queues/jobQueue.js
+++ server/src/queues/jobQueue.js
@@ -6,6 +6,7 @@ const redisConfig = {
     host: process.env.REDIS_HOST || 'localhost',
     port: process.env.REDIS_PORT || 6379,
     password: process.env.REDIS_PASSWORD || undefined,
+    tls: process.env.REDIS_TLS === 'true' ? {} : undefined,
   },
 };
 
